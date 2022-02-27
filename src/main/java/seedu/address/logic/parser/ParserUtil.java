73cb4568c9a47a4a9bdcb8534fb8da6f27d56895
Kidsnd274 <samuel_tee_gy@hotmail.com>
2022-02-27 23:55:26 +0800

    Fully finalize remark feature Made changes to multiple classes to ensure that remark feature works with other commands

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -13,6 +13,7 @@ import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
+import seedu.address.model.person.Remark;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -78,6 +79,17 @@ public class ParserUtil {
             throw new ParseException(Address.MESSAGE_CONSTRAINTS);
         }
         return new Address(trimmedAddress);
+    }
+
+    /**
+     * Parses a {@code String address} into an {@code Address}.
+     * Leading and trailing whitespaces will be trimmed.
+     *
+     * @throws ParseException if the given {@code address} is invalid.
+     */
+    public static Remark parseRemark(String remarkString) {
+        requireNonNull(remarkString);
+        return new Remark(remarkString);
     }
 
     /**
